89db95eb9256a319a70f159e371a7fbea75b143d
danieljkee <sdanser1@bk.ru>
2023-01-20 18:52:35 +0700

    add DELETE operation, add comments describe work of code

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ from flask import Flask
 from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
 
+# create the app
 app = Flask(__name__)
 api = Api(app)
+# configure the SQLite database, relative to the app instance folder
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+# initialize the app with the extension
 db = SQLAlchemy(app)
 
 
+# define a model class using subclass db.Model
 class VideoModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
@@ -18,7 +22,11 @@ class VideoModel(db.Model):
         return f"Video (name = {name}, views = {views}, likes = {likes}"
 
 
-db.create_all()
+# after all models and tables are defined, call SQLAlchemy.create_all()
+# to create the table schema in the database.
+# DO IT ONCE! and then comment below code string
+# db.create_all()
+
 
 video_put_args = reqparse.RequestParser()
 video_put_args.add_argument("name", type=str, help="Name of the video is required", required=True)
@@ -54,8 +62,8 @@ class Video(Resource):
             abort(409, message="Video id taken...")
 
         video = VideoModel(id=video_id, name=args["name"], views=args["views"], likes=args["likes"])
-        db.session.add(video)
-        db.session.commit()
+        db.session.add(video)  # adds an object to the session, to be inserted
+        db.session.commit()  # remember to call after modifying, adding, or deleting any data
         return video, 201
 
     @marshal_with(resource_fields)
@@ -77,8 +85,15 @@ class Video(Resource):
 
         return result
 
+    @marshal_with(resource_fields)
     def delete(self, video_id):
-        return "", 204  # 204 means deleted successfully
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+            abort(404, message="Could not find video with that id"
+                               " you want to delete")
+        db.session.delete(result)
+        db.session.commit()
+        return "Video deleted successfully", 204  # 204 means deleted successfully
 
 
 api.add_resource(Video, "/video/<int:video_id>")
