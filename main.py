6eb6ba283d0e1b63a31b15fb6e9268eb4db73c5a
danieljkee <sdanser1@bk.ru>
2023-01-20 13:42:20 +0700

    add patch operation with DB

--- main.py
+++ main.py
@@ -25,6 +25,11 @@ video_put_args.add_argument("name", type=str, help="Name of the video is require
 video_put_args.add_argument("views", type=int, help="Views of the video is required", required=True)
 video_put_args.add_argument("likes", type=int, help="Likes of the video is required", required=True)
 
+video_update_args = reqparse.RequestParser()
+video_update_args.add_argument("name", type=str, help="Name of the video is required")
+video_update_args.add_argument("views", type=int, help="Views of the video is required")
+video_update_args.add_argument("likes", type=int, help="Likes of the video is required")
+
 resource_fields = {
     "id": fields.Integer,
     "name": fields.String,
@@ -52,6 +57,25 @@ class Video(Resource):
         db.session.add(video)
         db.session.commit()
         return video, 201
+
+    @marshal_with(resource_fields)
+    def patch(self, video_id):
+        args = video_update_args.parse_args()
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+            abort(404, message="Video doesn't exist, cannot update")
+
+        if args["name"]:
+            result.name = args["name"]
+        if args["views"]:
+            result.views = args["views"]
+        if args["likes"]:
+            result.likes = args["likes"]
+
+        db.session.add(result)
+        db.session.commit()
+
+        return result
 
     def delete(self, video_id):
         return "", 204  # 204 means deleted successfully
